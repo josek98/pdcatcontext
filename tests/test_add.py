675aa8028bef57e19e7a00c535c1474e40b1b38b
josek98 <josemmsscc98@gmail.com>
2025-02-02 14:27:58 +0100

    add more testing

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,8 +1,11 @@
 import pandas as pd  # type: ignore
+import pytest
 from pdcatcontext import CatContext
 
 
-def df_init() -> pd.DataFrame:
+# region Fixtures
+@pytest.fixture
+def sample_df() -> pd.DataFrame:
     df_ = pd.DataFrame(
         {
             "A": ["a", "b", "c"],
@@ -14,14 +17,26 @@ def df_init() -> pd.DataFrame:
     return df_
 
 
-class TestAdd:
-    def test_concat_string_columns(self):
-        df = df_init()
-        df_copy = df.copy()
-        df_copy["E"] = df_copy["A"] + df_copy["B"]
-        with CatContext(["df"]):
-            df["E"] = df["A"] + df["B"]
+# region Tests
+def test_concat_string_columns(sample_df):
+    df = sample_df
+    df_copy = df.copy()
+    df_copy["E"] = df_copy["A"] + df_copy["B"]
+    with CatContext(["df"]):
+        df["E"] = df["A"] + df["B"]
 
-        assert df.equals(
-            df_copy.astype({"A": "category", "B": "category", "E": "category"})
-        )
+    assert df.equals(
+        df_copy.astype({"A": "category", "B": "category", "E": "category"})
+    )
+
+
+def test_concat_string_column_with_string(sample_df):
+    df = sample_df
+    df_copy = df.copy()
+    df_copy["E"] = df_copy["B"] + "testing"
+    with CatContext(["df"]):
+        df["E"] = df["B"] + "testing"
+
+    assert df.equals(
+        df_copy.astype({"A": "category", "B": "category", "E": "category"})
+    )
